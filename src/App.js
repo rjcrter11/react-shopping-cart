56ab006db8d7b2ecadef675fee0bbf98afefcc4a
rjcrter11 <rjcrter11@gmail.com>
2020-02-20 13:40:39 -0600

    mvp met. Context is easier than thinking through redux...

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -21,12 +22,12 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-
-        {/* Routes */}
-        <Route exact path="/" component={Products} />
-
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={{ cart }}>
+          <Navigation />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
